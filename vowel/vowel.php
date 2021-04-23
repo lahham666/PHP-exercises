c53e6a7bc0cd3ded171812017a46f0b74c02f5d4
lahham666 <65540932+lahham666@users.noreply.github.com>
2021-04-23 22:56:53 +0300

    Add files via upload

---
+++ vowel/vowel.php
@@ -0,0 +1,45 @@
+<?php
+
+$char = $_GET['sa'];
+$resStr = strtolower($char);
+
+
+switch($resStr)
+{
+
+   case 'a':
+    echo "Its Vowel";
+    break;
+
+    case 'e' :
+        echo "Its Vowel";
+        break;
+
+        case 'i' :
+            echo "Its Vowel";
+            break;
+
+            case 'o' :
+                echo "Its Vowel";
+                break;
+                     case 'u' :
+                       echo "Its Vowel";
+                         break;
+                            
+                         case 'y' :
+                            echo "Its Vowel";
+                              break;
+
+                              case 'w' :
+                                echo "Its Vowel";
+                                  break;
+         
+                                  default: echo "Not Vowel";
+
+
+
+
+}
+
+
+?>
